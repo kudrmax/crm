939a1e5d0c93d79d1cab89f26aec9ff465153883
kudrmax <mdkudryashov@edu.hse.ru>
2024-09-06 15:59:03 +0300

    Add requests in add_user

--- src/bot/handlers/add_user.py
+++ src/bot/handlers/add_user.py
@@ -1,7 +1,6 @@
-from enum import Enum
+import json
 
-from aiogram import Router
-import asyncio
+import requests
 from aiogram import Bot, Dispatcher, types, Router
 from aiogram.filters import Command, StateFilter
 from aiogram.fsm.context import FSMContext
@@ -14,6 +13,8 @@ from src.bot.keyboards.simple_row_by_list import make_row_keyboard_by_list
 
 router = Router()
 
+BASE_URL = 'http://0.0.0.0:8000'
+
 
 class AddUserState(StatesGroup):
     waiting_for_name = State()
@@ -22,11 +23,27 @@ class AddUserState(StatesGroup):
     waiting_new_field_value = State()
 
 
-class UserFieldEnum(Enum):
-    name = 'Имя'
-    telegram = 'Telegram'
-    phone = 'Телефон'
-    birthday = 'Дата рождения'
+# class UserFieldEnum(Enum):
+#     name = 'Имя'
+#     telegram = 'Telegram'
+#     phone = 'Телефон'
+#     birthday = 'Дата рождения'
+
+
+from_button_text_to_database_fields = {
+    'Имя': 'name',
+    'Telegram': 'telegram',
+    'Телефон': 'phone',
+    'Дата рождения': 'birthday',
+}
+
+
+# user_fields = [
+#     ('name', 'Имя'),
+#     ('telegram', 'Telegram'),
+#     ('phone', 'Телефон'),
+#     ('birthday', 'Дата рождения'),
+# ]
 
 
 def make_edit_or_finish_user_menu():
@@ -38,7 +55,7 @@ def make_edit_or_finish_user_menu():
 
 def make_edit_user_menu():
     return make_row_keyboard_by_list([
-        *[obj.value for obj in UserFieldEnum],
+        *from_button_text_to_database_fields.keys(),
         'Отменить',
     ])
 
@@ -51,8 +68,15 @@ async def add_user(message: types.Message, state: FSMContext):
 
 @router.message(AddUserState.waiting_for_name)
 async def process_name(message: types.Message, state: FSMContext):
-    name = message.text
-    await state.update_data(name=name)
+    input_name = message.text
+    res_post = requests.post(
+        BASE_URL + '/contacts',
+        data=json.dumps({"name": input_name})
+    )
+    id = res_post.json()["id"]
+    await state.update_data(id=id)
+    res_get = requests.get(BASE_URL + f'/contacts/{id}')
+    name = res_get.json()['name']
     await message.answer(
         f"Добавлен пользователь: {name}",
         reply_markup=make_edit_or_finish_user_menu()
@@ -64,25 +88,29 @@ async def process_name(message: types.Message, state: FSMContext):
 async def process_user_option(message: types.Message, state: FSMContext):
     button_text = message.text
     user_data = await state.get_data()
-    # name = user_data.get('name')
 
     if button_text == 'Редактировать пользователя':
         await message.answer(
             "Выберите, что вы хотите редактировать:",
-            reply_markup=make_row_keyboard_by_list([
-                *[obj.value for obj in UserFieldEnum],
-                'Отменить',
-            ])
+            reply_markup=make_edit_user_menu()
         )
         await state.set_state(AddUserState.waiting_for_edit)
         return
 
     if button_text == 'Завершить':
         # Формируем сообщение с данными пользователя
-        fields = ['name', 'telegram', 'phone', 'email', 'birthdate']
-        user_info = {field: user_data.get(field, 'Не указано') for field in fields}
-        user_info_message = "\n".join([f"{field.capitalize()}: {value}" for field, value in user_info.items()])
-        await message.answer(f"Вы добавили пользователя:\n{user_info_message}", reply_markup=make_main_menu())
+        # fields = from_button_text_to_database_fields.keys()
+        # user_info = {field: user_data.get(field, 'Не указано') for field in fields}
+        # user_info_message = "\n".join([f"{field.capitalize()}: {value}" for field, value in user_info.items()])
+        id = (await state.get_data()).get('id')
+        contact = requests.get(BASE_URL + f'/contacts/{id}').json()
+        user_info_message = "\n".join([
+            f"{field}: {value}" for field, value in contact.items()
+        ])
+        await message.answer(
+            f"Вы добавили пользователя:\n{user_info_message}",
+            reply_markup=make_main_menu()
+        )
         await state.clear()
         return
 
@@ -96,12 +124,12 @@ async def edit_user(message: types.Message, state: FSMContext):
         await state.set_state(AddUserState.waiting_for_choosing_edit_or_finish)
         return
 
-    if button_text in [obj.value for obj in UserFieldEnum]:
+    if button_text in from_button_text_to_database_fields.keys():
         await message.answer(
-            f"Введите новый {button_text.lower()}:",
+            f'Введите новый "{button_text}":',
             reply_markup=ReplyKeyboardRemove()
         )
-        await state.update_data(current_field=button_text.lower())
+        await state.update_data(current_field=from_button_text_to_database_fields[button_text])
         await state.set_state(AddUserState.waiting_new_field_value)
         return
 
@@ -112,13 +140,17 @@ async def edit_user(message: types.Message, state: FSMContext):
 async def update_field_value(message: types.Message, state: FSMContext):
     user_data = await state.get_data()
     current_field = user_data.get('current_field')
+    id = user_data.get('id')
 
     if current_field:
-        await state.update_data(**{current_field: message.text})
-        await message.answer(
-            f"{current_field.capitalize()} обновлен на {message.text}.",
-            reply_markup=make_edit_or_finish_user_menu()
-        )
+        contact = requests.put(BASE_URL + f'/contacts/{id}', data=json.dumps({current_field: message.text})).json()
+        try:
+            await message.answer(
+                f"{current_field} обновлен на {contact[current_field]}.",
+                reply_markup=make_edit_or_finish_user_menu()
+            )
+        except KeyError:
+            print(contact)
         await state.set_state(AddUserState.waiting_for_choosing_edit_or_finish)
         return
 
