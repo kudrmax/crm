c22900b0be7e2e7bc12124a8859c85aa8d255c79
kudrmax <mdkudryashov@edu.hse.ru>
2024-09-17 17:43:28 +0300

    Add API /stats/days_count_since_last_interaction

--- src/api/stats/dao.py
+++ src/api/stats/dao.py
@@ -1,6 +1,6 @@
 from typing import List
 
-from sqlalchemy import select, func, distinct
+from sqlalchemy import select, func, distinct, String, cast
 
 from src.api.contacts.models import MContact
 from src.api.dao_base import DAO
@@ -9,20 +9,54 @@ from src.api.stats.schemas import SDayCount
 
 
 class DAOStats(DAO):
-    async def get_stat_count_of_interasions(self) -> List[SDayCount]:
+    async def get_stat_count_of_interactions(self, name: str | None = None) -> List[SDayCount] | SDayCount:
         SQL = """
-        SELECT clogs.contact_id AS contact_id, contacts.name AS name, COUNT(DISTINCT DATE(datetime)) AS day_count
+        SELECT 
+            logs.contact_id AS contact_id,
+            contacts.name AS name, 
+            COUNT(DISTINCT DATE(datetime)) AS day_count
         FROM contacts
         JOIN logs
             ON contacts.id = logs.contact_id
         GROUP BY logs.contact_id;
         """
-
         query = (
             select(
                 MLog.contact_id.label('contact_id'),
                 MContact.name.label('name'),
                 func.count(distinct(func.date(MLog.datetime))).label('day_count')
+            )
+            .join(MContact, MContact.id == MLog.contact_id)
+            .group_by(MLog.contact_id, MContact.name)
+        )
+        rows = (await self.db.execute(query)).all()
+        data = [SDayCount(name=row[1], day_count=row[2]) for row in rows]
+        # if name:
+        #     for name, day_count in data:
+        #         if name == name:
+        #             return SDayCount(name=name, day_count=day_count)
+        #     return SDayCount(name=name, day_count=-1)
+
+        return data
+
+    async def get_days_count_since_last_interaction(self, name: str | None = None) -> List[SDayCount] | SDayCount:
+        SQL = """
+        SELECT 
+            logs.contact_id AS contact_id,
+            contacts.name AS name,
+            DATE_PART('day', CURRENT_DATE - MAX(DATE(logs.datetime))) AS days_since_last_interaction
+        FROM contacts
+        JOIN logs ON contacts.id = logs.contact_id
+        GROUP BY logs.contact_id, contacts.name;
+        """
+        query = (
+            select(
+                MLog.contact_id.label('contact_id'),
+                MContact.name.label('name'),
+                func.date_part(
+                    'day',
+                    func.age(func.current_date(), func.max(func.date(MLog.datetime)))
+                ).label('days_since_last_interaction')
             )
             .join(MContact, MContact.id == MLog.contact_id)
             .group_by(MLog.contact_id, MContact.name)
--- src/api/stats/router.py
+++ src/api/stats/router.py
@@ -10,8 +10,32 @@ router = APIRouter(
     tags=["Statistics"],
 )
 
+
 @router.get("/count_of_interactions")
-async def get_stats(
+async def get_count_of_interactions(
         dao: DAOStats = Depends()
 ) -> List[SDayCount]:
-    return await dao.get_stat_count_of_interasions()
+    return await dao.get_stat_count_of_interactions()
+
+
+@router.get("/count_of_interactions/{name}")
+async def get_count_of_interactions(
+        name: str,
+        dao: DAOStats = Depends()
+) -> List[SDayCount]:
+    return await dao.get_stat_count_of_interactions(name)
+
+
+@router.get("/days_count_since_last_interaction")
+async def days_count_since_last_interaction(
+        dao: DAOStats = Depends()
+) -> List[SDayCount]:
+    return await dao.get_days_count_since_last_interaction()
+
+
+@router.get("/days_count_since_last_interaction/{name}")
+async def days_count_since_last_interaction(
+        name: str,
+        dao: DAOStats = Depends()
+) -> List[SDayCount]:
+    return await dao.get_days_count_since_last_interaction(name)
