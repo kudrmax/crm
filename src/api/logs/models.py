dbb1b40fc14a60089d55a9008c8b79e46f8e24f1
kudrmax <mdkudryashov@edu.hse.ru>
2024-09-17 17:20:58 +0300

    Clear code

--- src/api/logs/models.py
+++ src/api/logs/models.py
@@ -11,6 +11,5 @@ class MLog(Base):
 
     id = Column(Integer, primary_key=True)
     contact_id = Column(UUID(as_uuid=True), ForeignKey('contacts.id', ondelete="CASCADE"), nullable=False)
-    # datetime = Column(DateTime, server_default=func.current_timestamp(), nullable=False)
     datetime = Column(DateTime(timezone=True), default=lambda: datetime.datetime.now(datetime.timezone.utc))
     log = Column(String, nullable=False, default="")
