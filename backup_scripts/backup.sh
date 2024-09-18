392b10a7517f81031c0720ac1dd2c158a4d1a572
kudrmax <mdkudryashov@edu.hse.ru>
2024-09-18 12:46:47 +0300

    Add backup.sh and restore.sh scripts for Postgreds

---
+++ backup_scripts/backup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Скрипт делает бекап в папке ../backups
+
+# Определите переменные
+CONTAINER_NAME="crm"
+TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
+BACKUP_FILE="../backups/crm_backup_$TIMESTAMP.dump"
+BACKUP_DIR="/backup"
+
+# Создайте директорию для бекапов в контейнере
+docker exec -t $CONTAINER_NAME mkdir -p $BACKUP_DIR
+
+# Выполните бекап базы данных
+docker exec -t $CONTAINER_NAME pg_dump -U postgres -F c -b -v -f $BACKUP_DIR/crm_backup.dump crm
+
+# Скопируйте бекап из контейнера на хост
+docker cp $CONTAINER_NAME:$BACKUP_DIR/crm_backup.dump $BACKUP_FILE
+
+# Удалите бекап из контейнера после копирования
+docker exec -t $CONTAINER_NAME rm $BACKUP_DIR/crm_backup.dump
+
+echo "Backup completed and saved to $BACKUP_FILE"
---
+++ backup_scripts/restore.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Скрипт восстанавливает последний бекап из папки ../backups
+
+
+# Определите переменные
+CONTAINER_NAME="crm"
+BACKUP_DIR="../backups"
+BACKUP_FILE=$(ls -t $BACKUP_DIR/crm_backup_*.dump | head -n 1)
+BACKUP_FILE_NAME=$(basename $BACKUP_FILE)
+BACKUP_DIR_IN_CONTAINER="/backup"
+
+# Проверьте, что бекап существует
+if [ -z "$BACKUP_FILE" ]; then
+    echo "No backup files found."
+    exit 1
+fi
+
+# Остановите и удалите контейнер, если он запущен
+docker stop $CONTAINER_NAME
+docker rm $CONTAINER_NAME
+
+# Запустите новый контейнер с базой данных
+docker-compose up -d db
+
+# Создайте директорию для бекапов в контейнере
+docker exec -t $CONTAINER_NAME mkdir -p $BACKUP_DIR_IN_CONTAINER
+
+# Скопируйте бекап в контейнер
+docker cp $BACKUP_FILE $CONTAINER_NAME:$BACKUP_DIR_IN_CONTAINER/crm_backup.dump
+
+# Восстановите базу данных из бекапа
+docker exec -t $CONTAINER_NAME pg_restore -U postgres -d crm -v $BACKUP_DIR_IN_CONTAINER/crm_backup.dump
+
+# Удалите бекап из контейнера после восстановления
+docker exec -t $CONTAINER_NAME rm $BACKUP_DIR_IN_CONTAINER/crm_backup.dump
+
+echo "Restore completed from $BACKUP_FILE_NAME"
