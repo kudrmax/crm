6da793b76310920114e8dac64b3cd54026b4a7d2
kudrmax <mdkudryashov@edu.hse.ru>
2024-09-25 20:33:31 +0300

    Add emoji to log buttons

--- src/bot/keyboards/keyboards.py
+++ src/bot/keyboards/keyboards.py
@@ -28,7 +28,7 @@ def make_keyboard_by_lists(items: list[list[str]]) -> ReplyKeyboardMarkup:
 
 def make_contact_profile_kb():
     return make_keyboard_by_lists([
-        ['Start logging', 'Get logs'],
+        ['Start logging ⬆️', 'Get logs ⬇️'],
         ['Edit logs', 'Add empty log'],
         ['Edit contact', 'Delete contact', 'Go to main menu']
     ])
