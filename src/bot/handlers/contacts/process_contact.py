664dabd6333026a5b88c8b73867254b29d99c0ff
kudrmax <mdkudryashov@edu.hse.ru>
2024-09-26 23:47:43 +0300

    Add getting logs before start logging

--- src/bot/handlers/contacts/process_contact.py
+++ src/bot/handlers/contacts/process_contact.py
@@ -25,6 +25,7 @@ async def get_logs_handler(message: Message, state: FSMContext):
 
 @router.message(ContactProfileState.choose_action, F.text == 'Start logging ⬆️')
 async def add_log(message: Message, state: FSMContext):
+    await get_logs(message, state)
     await start_logging(
         message=message,
         state=state,
