05b586e3046476d6086306144b45feddf611fef3
kudrmax <mdkudryashov@edu.hse.ru>
2024-10-05 13:44:39 +0300

    Fix host and port in main.py

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ def root():
 if __name__ == '__main__':
     uvicorn.run(
         'main:app',
-        host=settings.server.host,
-        port=settings.server.port,
+        host='0.0.0.0',
+        port=8000,
         reload=True
     )
