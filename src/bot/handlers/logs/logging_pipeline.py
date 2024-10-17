e1fe86abb0deb9b9c9f5e448043257d48ac6a4e6
kudrmax <mdkudryashov@edu.hse.ru>
2024-10-17 13:57:21 +0300

    Add pipeline to btn in logging

--- src/bot/handlers/logs/logging_pipeline.py
+++ src/bot/handlers/logs/logging_pipeline.py
@@ -5,6 +5,7 @@ from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import StatesGroup
 from aiogram.types import Message
 
+from src.bot.handlers.contacts.search_contact_pipeline import search_contact_from_main_to_profile
 from src.bot.helper import Helper
 from src.bot.keyboards import make_row_keyboard_by_list, logging_kb
 from src.bot.states import AddLog
@@ -39,6 +40,12 @@ async def set_date_to_yesterday(message: Message, state: FSMContext):
 async def set_date_to_today(message: Message, state: FSMContext):
     await state.update_data(date=None)
     await message.answer('Date was set to today')
+
+
+@router.message(AddLog.logging, F.text.lower().contains('find contact'))
+async def find_contact(message: Message, state: FSMContext):
+    await state.clear()
+    await search_contact_from_main_to_profile(message, state)
 
 
 @router.message(AddLog.logging, F.text.lower().contains('stop logging'))
