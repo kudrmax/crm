062c63215debbaf4044ae091fa4475d227f354d5
Maksim Kudryashov <mdmkudryashov@avito.ru>
2025-01-26 23:39:08 +0300

    Switch to service: delete contact

--- src/bot/handlers/contacts/delete_contact_pipeline.py
+++ src/bot/handlers/contacts/delete_contact_pipeline.py
@@ -6,6 +6,7 @@ from src.bot.helper import Helper
 from src.bot.keyboards import contact_profile_kb, main_kb
 from src.bot.states import DeleteContactState, ContactProfileState
 from src.errors import ContactNotFoundError
+from src.services.contact_log.service import contact_log_service
 
 router = Router()
 
@@ -20,14 +21,15 @@ async def choose_action(message: Message, state: FSMContext):
 async def delete(message: Message, state: FSMContext):
     data = await state.get_data()
     name = data['name']
+
     confirm_text = f'I want to delete contact {name}'
     if message.text != confirm_text:
         await message.answer(f'You text is not "{confirm_text}". Try again or cancel:')
         return
-    else:
-        try:
-            await Helper.delete_contact(name)
-            await message.answer(f"Contact with name {name} was deleted.", reply_markup=main_kb())
-            await state.clear()
-        except ContactNotFoundError:
-            await message.answer(f"Contact with name {name} not found.")
+
+    try:
+        contact_log_service.delete_contact_by_name(name)
+        await message.answer(f"Contact with name {name} was deleted.", reply_markup=main_kb())
+        await state.clear()
+    except ContactNotFoundError:
+        await message.answer(f"Contact with name {name} not found.")
