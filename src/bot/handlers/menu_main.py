e3dc28b7a558dc6adfe0ae49513dffd9889c16fb
Maksim Kudryashov <mdmkudryashov@avito.ru>
2025-01-27 16:03:23 +0300

    Refactor: move create contact pipeline

--- src/bot/handlers/menu_main.py
+++ src/bot/handlers/menu_main.py
@@ -3,7 +3,7 @@ from aiogram.filters import Command, StateFilter
 from aiogram.fsm.context import FSMContext
 from aiogram.types import Message
 
-from src.bot.handlers.contacts.create_contact_pipline import start_create_contact_pipeline
+from src.bot.handlers.pipelines.create_contact import start_create_contact_pipeline
 from src.bot.handlers.contacts.search_contact_pipeline import search_contact, search_contact_from_main_to_profile
 from src.bot.handlers.logs.get_last_logs_pipeline import get_last_logs
 from src.bot.keyboards import main_kb, contact_profile_kb, make_row_keyboard_by_list, make_keyboard_by_lists, stats_kb
--- src/bot/routers.py
+++ src/bot/routers.py
@@ -4,7 +4,7 @@ from src.bot.error_handlers import router as error_handlers
 
 from src.bot.handlers.menu_main import router as menu_main
 
-from src.bot.handlers.contacts.create_contact_pipline import router as create_contact_pipeline
+from src.bot.handlers.pipelines.create_contact import router as create_contact_pipeline
 from src.bot.handlers.contacts.delete_contact_pipeline import router as delete_contact_pipeline
 from src.bot.handlers.contacts.edit_contact_pipeline import router as edit_contact_pipeline
 from src.bot.handlers.contacts.menu_contact_profile import router as menu_contact_profile
