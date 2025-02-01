ee609cdf6c3d5bc4b49df67eeb93165d1f777d28
Maksim Kudryashov <mdmkudryashov@avito.ru>
2025-02-01 18:55:29 +0300

    Add: gpt service

---
+++ src/services/gpt/service.py
@@ -0,0 +1,79 @@
+from __future__ import annotations
+
+import dataclasses
+from typing import List, Any, Tuple
+import config
+
+from src.models.log.models import MLog
+
+from yandex_cloud_ml_sdk import YCloudML
+
+from src.services.voice2text.decorators import print_decorator
+
+
+@dataclasses.dataclass
+class GPTResult:
+    input: Any
+    output: str
+
+
+class GPTService:
+    def __init__(self):
+        self.sdk = YCloudML(
+            folder_id=config.folder_id,
+            auth=config.api_key,
+        )
+
+    def _create_request(self, messages) -> GPTResult:
+        result = self.sdk.models.completions("yandexgpt").configure(temperature=0.5).run(messages)
+
+        text = result.alternatives[0].text
+
+        return GPTResult(
+            output=text,
+            input=messages,
+        )
+
+    def convert_text_to_bullet_logs(self, text: str) -> Tuple[str, GPTResult]:
+        messages = [
+            {
+                "role": "system",
+                "text":
+                    """
+                    Преобразуй следующий текст в структурированный bullet список фактов о человеке.
+                    Факты должны быть краткими, без лишних вводных слов, но содержательными.
+                    Если в тексте есть важные акценты (например, что-то показалось важным собеседнику), отмечай их знаком "!" перед фактом.
+                    
+                    Формат ответа:
+                    — [Краткий факт]
+                    — ! [Факт, который имеет особое значение]
+                    — [Краткий факт]
+                    
+                    Если в тексте упоминаются другие люди или цифры (зарплаты, даты), сохрани их точность. 
+                    Если факт содержит личное мнение человека, отметь это явно (например, "Считает, что ...").
+                    """,
+            },
+            {
+                "role": "user",
+                "text": text,
+            },
+        ]
+        gpt_result = self._create_request(messages)
+        return gpt_result.output, gpt_result
+
+    def get_logs_from_text(self, text: str) -> List[MLog]:
+        pass
+
+
+gpt_service = GPTService()
+
+# text, result = s.convert_text_to_bullet_logs("""
+# Марко училась в Питере на программе, где сам создаёшь программу, и у неё направление в итоге было связано с когнитивистикой. Диплом написала, используя нейросети Torch, когнитивистику, суть была в том, что инцефалограммы или что-то такое сравнивало использование каких-то обычных нейронных сетей и графовых, у которых есть трейсинг. И вывод, трейсинг важен.
+# """)
+# print(text)
+# print(result)
+
+# text, result = s.convert_text_to_bullet_logs("""
+# Сейчас учится в школе 21 на направлении Data Science. Рассказала мне о том, что у неё есть знакомый, который работает во ВИТа на ГО с зарплатой 800 тысяч. Ещё рассказала, что... Неважно.""")
+# print(text)
+# print(result)
